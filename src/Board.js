1dadd215403d688f12b43d3b9cfa04f01aeb1d93
Siddharth Sriraman <siddharth18150@cse.ssn.edu.in>
2020-08-23 13:42:44 +0530

    working algorithm

--- src/Board.js
+++ src/Board.js
@@ -10,6 +10,9 @@ const genCoords = () => {
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
+const manhattanDist = (p1, p2) => {
+    return Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1])
+}
 
 const createQTable = () => {
     var arr = [];
@@ -61,27 +64,22 @@ class Board extends Component {
     }
 
     setDir (val) {
-        if(val === 37) {
-            if(this.state.direction !== 2)
-                this.setState({direction: 0});
+        if(this.state.dots.length === 2 && Math.abs(this.state.direction - (val - 37)) === 2) {
+            this.setState({direction: val - 37});            
+            return true;
         }
-        else if(val === 38) {
-            if(this.state.direction !== 3)
-                this.setState({direction: 1});
+        else if(val >= 37 && val <= 40) {
+            this.setState({direction: val - 37});
+            return false;
         }
-        else if(val === 39) {
-            if(this.state.direction !== 0)
-                this.setState({direction: 2});
-        }        
-        else if(val === 40) {
-            if(this.state.direction !== 1)
-                this.setState({direction: 3});
-        }        
     }
 
     onKeyDown = (e) => {
         e = e || window.event();
-        this.setDir(parseInt(e.keyCode));
+        // length 2 opposite direction exception 
+        if(this.setDir(parseInt(e.keyCode))) {
+            this.gameOver();
+        }
     }
 
     moveSnake = () => {
@@ -92,10 +90,7 @@ class Board extends Component {
         var valid = true;
         
         newx += dirs[state.direction][0];
-        newy += dirs[state.direction][1];
-
-        //newx += dirs[dir_][0];
-        //newy += dirs[dir_][1];        
+        newy += dirs[state.direction][1];    
 
         if(newx === state.food[0] && newy === state.food[1]) {
             while(true) {
@@ -136,14 +131,13 @@ class Board extends Component {
     }
 
     checkCollapsed = () => {
-        var state = this.state;
         var lost = false;
-        var head = state.dots[state.dots.length - 1];
-        state.dots.forEach((dot, i) => {
-            if(i !== 0 && i !== state.dots.length - 1 && head[0] === dot[0] && head[1] === dot[1]) {
+        var head = this.state.dots[this.state.dots.length - 1];
+        this.state.dots.forEach((dot, i) => {
+            if(i !== 0 && i !== this.state.dots.length - 1 && head[0] === dot[0] && head[1] === dot[1]) {
                 lost = true;
             }
-        })   
+        })
         return lost;
     }
 
@@ -163,18 +157,19 @@ class Board extends Component {
 
     action = (eps) => {
         var [surr, dir] = this.getState();
-        var v1 = surr[0] + 2 * surr[1] + 4 * surr[2] + 8 * surr[3];
+        var v1 = surr[0] + (2 * surr[1]) + (4 * surr[2]) + (8 * surr[3]);
         if(Math.random() < eps) {
             return Math.floor(Math.random() * 4);
         }
         else {
             var mx = -100000, ind = 0;
             for(var i = 0; i < 4; i++) {
-                if(Q_table[v1][dir][i] >= mx) {
+                if(Q_table[v1][dir][i] > mx) {
                     mx = Q_table[v1][dir][i];
                     ind = i;
                 }
             }
+            //console.log(surr, v1, dir, Q_table[v1][dir], ind);
             return parseInt(ind);
         }
     }
@@ -182,83 +177,94 @@ class Board extends Component {
     qlearning = async () => {
         var mx, done, reward;
         var next_surr, next_dir, next_v1;
-        var surr, dir, v1, dist;
+        var surr, dir, v1, dist, action, steps;
 
         var epsilon = this.state.epsilon;
         var discount_factor = this.state.discount_factor;
+        var dec = (0.9) / 90;
 
-        for(var ep = 0; ep < 200; ep++) {
+        for(var ep = 0; ep < 100; ep++) {
             done = false;
             [surr, dir] = this.getState();
-            v1 = surr[0] + 2 * surr[1] + 4 * surr[2] + 8 * surr[3];
+            v1 = surr[0] + (2 * surr[1]) + (4 * surr[2]) + (8 * surr[3]);
+            steps = 0;
+
             while(!done) {
-                dist = Math.abs(this.state.food[0] - this.state.dots[this.state.dots.length-1][0])
-                + Math.abs(this.state.food[1] - this.state.dots[this.state.dots.length-1][1]);
+                dist = manhattanDist(this.state.food, this.state.dots[this.state.dots.length - 1]);
  
                 // step
-                this.setDir(this.action(epsilon) + 37);
-                await delay(10);
-                done = this.moveSnake();
-
-                [next_surr, next_dir] = this.getState();
-                next_v1 = next_surr[0] + 2 * next_surr[1] + 4 * next_surr[2] + 8 * next_surr[3];
+                action = this.action(epsilon);
+                
+                // moving and checking the length 2 edge case:
+                if(this.setDir(action + 37)) done = true;
+                
+                if(ep >= 90)
+                    await delay(50);
+                else
+                    await delay(10);
+                
+                done = done || (steps >= 500) || this.moveSnake();
+                if(!done) {
+                    [next_surr, next_dir] = this.getState();
+                    next_v1 = next_surr[0] + (2 * next_surr[1]) + (4 * next_surr[2]) + (8 * next_surr[3]);
+                }
 
                 // reward
-                if(done) reward = -50;
-                else if(this.state.justAte === true) reward = 30;
-                else if(Math.abs(this.state.food[0] - this.state.dots[this.state.dots.length-1][0])
-                + Math.abs(this.state.food[1] - this.state.dots[this.state.dots.length-1][1]) < dist) 
-                    reward = 2;
-                else reward = -1;  
+                if(done) 
+                    reward = -100;
+                else if(this.state.justAte === true) 
+                    reward = 300;
+                else if(manhattanDist(this.state.food, this.state.dots[this.state.dots.length - 1]) < dist) 
+                    reward = 1;
+                else 
+                    reward = -10;  
 
-                //if(reward )
-                //console.log(reward)
-
-                mx = -100000;
-                for(var i = 0; i < 4; i++) {
-                    if(Q_table[next_v1][next_dir][i] >= mx) {
-                        mx = Q_table[next_v1][next_dir][i];
+                if(!done) {
+                    mx = -100000;
+                    for(var i = 0; i < 4; i++) {
+                        if(Q_table[next_v1][next_dir][i] >= mx) {
+                            mx = Q_table[next_v1][next_dir][i];
+                        }
                     }
-                }              
+                    
+                }          
+                else mx = 0;      
 
-                Q_table[v1][dir] += 0.01 * ((reward + discount_factor * mx) - Q_table[v1][dir])
+                Q_table[v1][dir][action] += 0.01 * ((reward + (discount_factor * mx)) - Q_table[v1][dir][action])
+                
+                v1 = next_v1;
+                dir = next_dir;
+                steps += 1
             }
-            this.gameOver();
-            if((epsilon * 0.99) >= 0.01) epsilon *= 0.996;
+            this.gameOver();    
+            if(epsilon - dec >= 0)
+                epsilon -= dec;
+            
             this.setState({...this.state, ep: ep+1, epsilon: epsilon})
         }
+        console.log(Q_table);
     }
 
     /*
         State definition:
-            surr = 3 cells around the head  0 - up  1 - left  2 - right
-            dir = relative pos of the apple (8 vals 0 - 7)
+            surr = 4 cells around the head  LURD
+            dir = relative pos of the apple (8 possible vals 0 - 7)
     */
     getState = () => {
         var surr = [0, 0, 0, 0];
-        var dir = 0, dir_ = this.state.direction;
+        var dir = 0;
         var head = this.state.dots[this.state.dots.length - 1]
         var relx = head[0] - this.state.food[0];
         var rely = head[1] - this.state.food[1];
 
-        if(relx < 0 && rely < 0) dir = 6;//dir = 0;
-        else if(relx === 0 && rely < 0) dir = 5; //dir = 6;
-        else if(relx > 0 && rely < 0) dir = 4;//dir = 2;
+        if(relx < 0 && rely < 0) dir = 6;
+        else if(relx === 0 && rely < 0) dir = 5;
+        else if(relx > 0 && rely < 0) dir = 4;
         else if(relx > 0 && rely === 0) dir = 3;
         else if(relx > 0 && rely > 0) dir = 2;
-        else if(relx === 0 && rely > 0) dir = 1;//dir = 5;
-        else if(relx < 0 && rely > 0) dir = 0;//dir = 6;
+        else if(relx === 0 && rely > 0) dir = 1;
+        else if(relx < 0 && rely > 0) dir = 0;
         else if(relx < 0 && rely === 0) dir = 7;
-
-        if(dir_ === 2) {           
-            //dir = (8 + dir - 6) % 8;
-        }
-        else if(dir_ === 0) {
-            //dir = (8 + dir - 2) % 8;
-        }
-        else if(dir_=== 3) {
-            //dir = (8 + dir - 4) % 8;
-        }
 
         for(var index = 0; index < 4; index++) {    
             if(checkBounds([head[0] + dirs[index][0],  head[1] + dirs[index][1]])) {
@@ -267,7 +273,7 @@ class Board extends Component {
             else {
                 // eslint-disable-next-line no-loop-func
                 this.state.dots.forEach((dot, i) => {
-                    if(i < this.state.dots.length - 2) {
+                    if(i <= this.state.dots.length - 2) {
                         if((dot[0] === (head[0] + dirs[index][0])) && (dot[1] === (head[1] + dirs[index][1]))) 
                             surr[index] = 1;
                     }
