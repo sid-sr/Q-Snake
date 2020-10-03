7d753877a1b3368b98076ff1feba8fe17b1432c1
Siddharth Sriraman <siddharth18150@cse.ssn.edu.in>
2020-10-03 20:07:33 +0530

    Updates

---
+++ precache-manifest.cfe0d8bdaed8c39328e1279d7e45977f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c0a70f1847c8ceca65fba5430318e8",
+    "url": "/Q-Snake/index.html"
+  },
+  {
+    "revision": "a5cccf9de539d5ca3e65",
+    "url": "/Q-Snake/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8b152c156695b27ffeb1",
+    "url": "/Q-Snake/static/css/main.47daf4cb.chunk.css"
+  },
+  {
+    "revision": "a5cccf9de539d5ca3e65",
+    "url": "/Q-Snake/static/js/2.8535c23f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Q-Snake/static/js/2.8535c23f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b152c156695b27ffeb1",
+    "url": "/Q-Snake/static/js/main.bc51f61f.chunk.js"
+  },
+  {
+    "revision": "b9d02ca916ae38095b3c",
+    "url": "/Q-Snake/static/js/runtime-main.ddf70c5a.js"
+  },
+  {
+    "revision": "58c351083372d8efd77d9cd08feebd8f",
+    "url": "/Q-Snake/static/media/FacileSans.58c35108.ttf"
+  },
+  {
+    "revision": "e53d843ba6016ec0326ad744e61287f9",
+    "url": "/Q-Snake/static/media/cyborg-25.e53d843b.png"
+  }
+]);
